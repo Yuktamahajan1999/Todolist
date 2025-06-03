ace10275a73513bf8fe00b916ecc114fdd630c9c
yukta mahajan <yuktamahajan404@gmail.com>
2025-06-03 13:39:42 +0530

    Updated Backend

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,12 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import todoRouter from './Router/todoRouter.js';
-import userRouter from "./Router/userRouter.js"; 
+import userRouter from "./Router/userRouter.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://todolist-lriy.vercel.app"],
+  origin: ["http://localhost:5173", "https://todolist-lriy.vercel.app", "https://todolist-lriy-git-main-yukta-mahajans-projects.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
@@ -19,10 +19,10 @@ app.use("/todo", todoRouter);
 app.use("/user", userRouter);
 
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB connected!'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
